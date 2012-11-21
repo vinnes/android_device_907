42c83f9915b7f452b2fe53c7038869a38a81860c
myfluxi <linflux@arcor.de>
2012-11-21 16:26:59 +0100

    power: Support ondemand governor
    Change-Id: I5f4a4fb868e7f424f817952ae9583b7e729ad58a

--- power/power_sun4i.c
+++ power/power_sun4i.c
@@ -25,8 +25,16 @@
 #include <hardware/hardware.h>
 #include <hardware/power.h>
 
-#define BOOSTPULSE_PATH "/sys/devices/system/cpu/cpufreq/interactive/boostpulse"
-#define MALI_BOOSTPULSE_PATH "/sys/devices/platform/mali_dev.0/boostpulse"
+#define SCALINGMAXFREQ "/sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq"
+#define SCALING_GOVERNOR "/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor"
+#define BOOSTPULSE_INTERACTIVE "/sys/devices/system/cpu/cpufreq/interactive/boostpulse"
+#define BOOSTPULSE_ONDEMAND "/sys/devices/system/cpu/cpufreq/ondemand/boostpulse"
+#define BOOSTPULSE_MALI "/sys/devices/platform/mali_dev.0/boostpulse"
+
+#define MAX_BUF_SZ  10
+
+static char screen_off_max_freq[MAX_BUF_SZ] = "696000";
+static char scaling_max_freq[MAX_BUF_SZ] = "1008000";
 
 struct sun4i_power_module {
     struct power_module base;
@@ -34,6 +42,34 @@ struct sun4i_power_module {
     int boostpulse_fd;
     int boostpulse_warned;
 };
+
+static int sysfs_read(char *path, char *s, int num_bytes)
+{
+    char buf[80];
+    int count;
+    int ret = 0;
+    int fd = open(path, O_RDONLY);
+
+    if (fd < 0) {
+        strerror_r(errno, buf, sizeof(buf));
+        ALOGE("Error opening %s: %s\n", path, buf);
+
+        return -1;
+    }
+
+    if ((count = read(fd, s, num_bytes - 1)) < 0) {
+        strerror_r(errno, buf, sizeof(buf));
+        ALOGE("Error reading %s: %s\n", path, buf);
+
+        ret = -1;
+
+    } else
+        s[count] = '\0';
+
+    close(fd);
+
+    return ret;
+}
 
 static void sysfs_write(char *path, char *s)
 {
@@ -56,23 +92,66 @@ static void sysfs_write(char *path, char *s)
     close(fd);
 }
 
+static int get_scaling_governor(char governor[], int size) {
+    if (sysfs_read(SCALING_GOVERNOR, governor, size) == -1) {
+        return -1;
+
+    } else {
+        int len = strlen(governor);
+
+        len--;
+        while (len >= 0 && (governor[len] == '\n'
+			 || governor[len] == '\r'))
+            governor[len--] = '\0';
+    }
+
+    return 0;
+}
+
 static void sun4i_power_init(struct power_module *module)
 {
-    /*
-     * cpufreq interactive governor: timer 20ms, min sample 60ms,
-     * hispeed 700MHz at load 50%.
-     */
+    struct sun4i_power_module *sun4i;
+    char governor[80];
 
-    sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/timer_rate",
-                "20000");
-    sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/min_sample_time",
-                "60000");
-    sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/hispeed_freq",
-                "696000");
-    sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/go_hispeed_load",
-                "50");
-    sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/above_hispeed_delay",
-                "100000");
+    if (get_scaling_governor(governor, sizeof(governor)) < 0) {
+        ALOGE("Can't read scaling governor.");
+        sun4i->boostpulse_warned = 1;
+
+        return;
+    }
+
+    if (strncmp(governor, "interactive", sizeof(governor)) == 0) {
+
+        /*
+         * cpufreq interactive governor: timer 20ms, min sample 60ms,
+         * hispeed 700MHz at load 50%.
+         */
+
+        sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/timer_rate",
+                    "20000");
+        sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/min_sample_time",
+                    "60000");
+        sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/hispeed_freq",
+                    "696000");
+        sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/go_hispeed_load",
+                    "50");
+        sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/above_hispeed_delay",
+                    "100000");
+
+    } else if (strncmp(governor, "ondemand", sizeof(governor)) == 0) {
+
+        /*
+         * cpufreq ondemand governor: boostfreq 696MHz, up threshold 70%,
+         * sampling rate 50000.
+         */
+
+        sysfs_write("/sys/devices/system/cpu/cpufreq/ondemand/boostfreq",
+                    "696000");
+        sysfs_write("/sys/devices/system/cpu/cpufreq/ondemand/up_threshold",
+                    "70");
+        sysfs_write("/sys/devices/system/cpu/cpufreq/ondemand/sampling_rate",
+                    "50000");
+    }
 
     /*
      * Mali boost rate: 1200MHz PLL / 400MHz Mali freq, duration
@@ -81,25 +160,31 @@ static void sun4i_power_init(struct power_module *module)
 
     sysfs_write("/sys/module/mali/parameters/mali_boost_rate",
                 "1200");
-
     sysfs_write("/sys/module/mali/parameters/mali_boost_duration",
                 "500");
-
 }
 
 static int boostpulse_open(struct sun4i_power_module *sun4i)
 {
     char buf[80];
+    char governor[80];
 
     pthread_mutex_lock(&sun4i->lock);
 
     if (sun4i->boostpulse_fd < 0) {
-        sun4i->boostpulse_fd = open(BOOSTPULSE_PATH, O_WRONLY);
+        if (get_scaling_governor(governor, sizeof(governor)) < 0) {
+            ALOGE("Can't read scaling governor.");
+            sun4i->boostpulse_warned = 1;
 
-        if (sun4i->boostpulse_fd < 0) {
-            if (!sun4i->boostpulse_warned) {
+        } else {
+            if (strncmp(governor, "interactive", sizeof(governor)) == 0)
+                sun4i->boostpulse_fd = open(BOOSTPULSE_INTERACTIVE, O_WRONLY);
+            else if (strncmp(governor, "ondemand", sizeof(governor)) == 0)
+                sun4i->boostpulse_fd = open(BOOSTPULSE_ONDEMAND, O_WRONLY);
+
+            if (sun4i->boostpulse_fd < 0 && !sun4i->boostpulse_warned) {
                 strerror_r(errno, buf, sizeof(buf));
-                ALOGE("Error opening %s: %s\n", BOOSTPULSE_PATH, buf);
+                ALOGE("Error opening boostpulse: %s\n", buf);
                 sun4i->boostpulse_warned = 1;
             }
         }
@@ -111,12 +196,28 @@ static int boostpulse_open(struct sun4i_power_module *sun4i)
 
 static void sun4i_power_set_interactive(struct power_module *module, int on)
 {
+    int len;
+    char buf[MAX_BUF_SZ];
+
     /*
      * Lower maximum frequency when screen is off.
      */
 
-    sysfs_write("/sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq",
-                on ? "1008000" : "696000");
+    if (!on) {
+        /* read the current scaling max freq and save it before updating */
+        len = sysfs_read(SCALINGMAXFREQ, buf, sizeof(buf));
+
+        /* make sure it's not the screen off freq, if the "on"
+         * call is skipped (can happen if you press the power
+         * button repeatedly) we might have read it. We should
+         * skip it if that's the case
+         */
+        if (len != -1 && strncmp(buf, screen_off_max_freq,
+                strlen(screen_off_max_freq)) != 0)
+            memcpy(scaling_max_freq, buf, sizeof(buf));
+        sysfs_write(SCALINGMAXFREQ, screen_off_max_freq);
+    } else
+        sysfs_write(SCALINGMAXFREQ, scaling_max_freq);
 }
 
 static void sun4i_power_hint(struct power_module *module, power_hint_t hint,
@@ -130,19 +231,25 @@ static void sun4i_power_hint(struct power_module *module, power_hint_t hint,
     switch (hint) {
     case POWER_HINT_INTERACTION:
     case POWER_HINT_CPU_BOOST:
-        if (data != NULL)
-            duration = (int) data;
+	if (boostpulse_open(sun4i) >= 0) {
+            if (data != NULL)
+	        duration = (int) data;
 
-        if (boostpulse_open(sun4i) >= 0) {
-            snprintf(buf, sizeof(buf), "%d", duration);
+	    snprintf(buf, sizeof(buf), "%d", duration);
 	    len = write(sun4i->boostpulse_fd, buf, strlen(buf));
 
 	    if (len < 0) {
 	        strerror_r(errno, buf, sizeof(buf));
-		ALOGE("Error writing to %s: %s\n", BOOSTPULSE_PATH, buf);
+	        ALOGE("Error writing to boostpulse: %s\n", buf);
+
+                pthread_mutex_lock(&sun4i->lock);
+                close(sun4i->boostpulse_fd);
+                sun4i->boostpulse_fd = -1;
+                sun4i->boostpulse_warned = 0;
+                pthread_mutex_unlock(&sun4i->lock);
 	    }
 
-	    sysfs_write(MALI_BOOSTPULSE_PATH, buf);
+	    sysfs_write(BOOSTPULSE_MALI, buf);
 	}
         break;
 
