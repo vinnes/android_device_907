2fd3e17a5b1b42e29fd88e51045815e8fa07fb20
Your Name <you@example.com>
2013-01-23 16:23:16 +0400

    Trying to Fix USB

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -55,9 +55,9 @@ ENABLE_WEBGL := true
 
 #Recovery Stuff
 #BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/softwinner/907/recovery_keys.c
-BOARD_UMS_LUNFILE := "/sys/class/android_usb/android0/f_mass_storage/lun/file"
-BOARD_SDCARD_INTERNAL_DEVICE := /dev/block/nandi
-BOARD_UMS_2ND_LUNFILE := "/sys/class/android_usb/android0/f_mass_storage/lun1/file"
+#BOARD_UMS_LUNFILE := "/sys/class/android_usb/android0/f_mass_storage/lun/file"
+#BOARD_SDCARD_INTERNAL_DEVICE := /dev/block/nandi
+#BOARD_UMS_2ND_LUNFILE := "/sys/class/android_usb/android0/f_mass_storage/lun1/file"
 #TARGET_RECOVERY_INITRC := device/softwinner/907/recovery_init.rc
 
 #twrp recovery
@@ -65,19 +65,20 @@ TARGET_RECOVERY_INITRC := device/softwinner/907/recovery_init.rc
 TARGET_RECOVERY_PIXEL_FORMAT := "RGB_565"
 DEVICE_RESOLUTION := 1024x768
 TW_FLASH_FROM_STORAGE := true
-#RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TW_NO_REBOOT_BOOTLOADER := true
 #TWRP_EVENT_LOGGING := true 
 TW_INTERNAL_STORAGE_PATH := "/emmc"
 TW_INTERNAL_STORAGE_MOUNT_POINT := "emmc"
 TW_EXTERNAL_STORAGE_PATH := "/sdcard"
 TW_EXTERNAL_STORAGE_MOUNT_POINT := "sdcard"
+RECOVERY_SDCARD_ON_DATA := true
+BOARD_UMS_LUNFILE := "/sys/devices/platform/msm_hsusb/gadget/lun0/file
 
 #Misc stuff
-TARGET_USE_CUSTOM_LUN_FILE_PATH = "/sys/class/android_usb/android0/f_mass_storage/lun%d/file"
-TARGET_USE_CUSTOM_SECOND_LUN_NUM := 1
+#TARGET_USE_CUSTOM_LUN_FILE_PATH = "/sys/class/android_usb/android0/f_mass_storage/lun%d/file"
+#TARGET_USE_CUSTOM_SECOND_LUN_NUM := 1
 TARGET_RECOVERY_PRE_COMMAND := "echo -n boot-recovery | busybox dd of=/dev/block/nandf count=1 conv=sync; sync"
 BOARD_USE_LEGACY_TOUCHSCREEN := true
-BOARD_USE_PREBUILT_TOUCHSCREEN := device/softwinner/907/modules/ft5x_ts.ko
 TARGET_HARDWARE_INCLUDE := $(TOP)/device/softwinner/907/libraries/include
 TARGET_PROVIDES_INIT_RC := true
 #TARGET_BOOTANIMATION_PRELOAD := true
