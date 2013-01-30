5b07f2b18a545516c08b6c2247c01805bf1adc4a
ugers <yugers@gmail.com>
2013-01-30 23:24:37 +0400

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -18,6 +18,7 @@ $(call inherit-product, device/softwinner/907/full_907.mk)
 
 # Inherit some common CM9 stuff.
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+$(call inherit-product, vendor/cm/config/gsm.mk)
 $(call inherit-product, device/softwinner/907/907-blobs.mk)
 
 ## Device identifier. This must come after all inclusions
@@ -27,4 +28,8 @@ PRODUCT_DEVICE := 907
 PRODUCT_MODEL := Nexus 7
 PRODUCT_MANUFACTURER := Asus
 PRODUCT_RELEASE_NAME := 907
+
+UTC_DATE := $(shell date +%s)
+DATE     := $(shell date +%Y%m%d)
+
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=nakasi TARGET_DEVICE=grouper BUILD_FINGERPRINT="google/nakasi/grouper:4.2.1/JOP40D/402395:user/release-keys" PRIVATE_BUILD_DESC="nakasi-user 4.2.1 JOP40D 402395 release-keys"
