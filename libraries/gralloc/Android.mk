e33ce2932185857c15d5e4522037820805a979c0
crass00 <crass00@hotmail.com>
2013-02-14 18:06:33 +0000

    fix compile error

--- libraries/gralloc/Android.mk
+++ libraries/gralloc/Android.mk
@@ -35,7 +35,6 @@ LOCAL_SHARED_LIBRARIES := liblog libcutils libMali libGLESv1_CM libUMP
 LOCAL_C_INCLUDES := device/softwinner/907/libraries/include
 
 LOCAL_CFLAGS := -DLOG_TAG=\"gralloc\" -DGRALLOC_32_BITS -DSTANDARD_LINUX_SCREEN
-endif
 
 LOCAL_SRC_FILES := \
 	gralloc_module.cpp \
