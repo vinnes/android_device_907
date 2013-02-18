3ffc7b659282ff5832c218185a7851919eeb6d01
ugers <yugers@gmail.com>
2013-02-18 10:49:48 +0400

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -53,13 +53,13 @@ BOARD_USE_SKIA_LCDTEXT := true
 BOARD_NO_ALLOW_DEQUEUE_CURRENT_BUFFER := true
 ENABLE_WEBGL := true
 
-#Recovery Stuff
+#CWM Recovery
 #BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/softwinner/907/recovery_keys.c
 BOARD_UMS_LUNFILE := "/sys/class/android_usb/android0/f_mass_storage/lun/file"
 BOARD_UMS_2ND_LUNFILE := "/sys/class/android_usb/android0/f_mass_storage/lun1/file"
 #TARGET_RECOVERY_INITRC := device/softwinner/907/recovery_init.rc
 
-#twrp recovery
+#TWRP recovery
 #BOARD_HAS_SDCARD_INTERNAL := true
 #TARGET_RECOVERY_INITRC := device/softwinner/907/recovery_init.rc
 #TARGET_RECOVERY_PIXEL_FORMAT := "RGB_565"
