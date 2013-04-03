eec770eaa8ee814fdd4c14dcb0ccc0e0ad2f240f
ugers <yugers@gmail.com>
2013-04-03 21:31:49 +0400

    Update hwccomposer_priv.h

--- libraries/hwcomposer/hwccomposer_priv.h
+++ libraries/hwcomposer/hwccomposer_priv.h
@@ -13,8 +13,8 @@
 
 #include <EGL/egl.h>
 
-#define  MAX_FBNUM		8
-#define  MAX_LAYERNUM	8
+#define  MAX_FBNUM        8
+#define  MAX_LAYERNUM    8
 
 typedef struct sun4i_hwc_layer
 {
@@ -22,36 +22,36 @@ typedef struct sun4i_hwc_layer
 
     uint32_t            dispW;
     uint32_t            dispH;
-    uint32_t			org_dispW;
-    uint32_t			org_dispH;
-    
-	uint32_t    		cropX;
-    uint32_t    		cropY;
-    uint32_t    		cropW;
-    uint32_t    		cropH;
-    
-    uint32_t    		posX;
-    uint32_t   	 		posY;
-    uint32_t    		posW;
-    uint32_t    		posH;
-    
-    uint32_t    		posX_last;
-    uint32_t    		posY_last;
-    uint32_t    		posW_last;
-    uint32_t    		posH_last;
+    uint32_t            org_dispW;
+    uint32_t            org_dispH;
 
-    uint32_t    		posX_org;
-    uint32_t    		posY_org;
-    uint32_t    		posW_org;
-    uint32_t    		posH_org;
-    
-    uint32_t			cur_framehandle;
-    uint32_t			screen;
-    uint32_t			currenthandle;
-	uint32_t			cur_frameid;
+    uint32_t            cropX;
+    uint32_t            cropY;
+    uint32_t            cropW;
+    uint32_t            cropH;
+
+    uint32_t            posX;
+    uint32_t                posY;
+    uint32_t            posW;
+    uint32_t            posH;
+
+    uint32_t            posX_last;
+    uint32_t            posY_last;
+    uint32_t            posW_last;
+    uint32_t            posH_last;
+
+    uint32_t            posX_org;
+    uint32_t            posY_org;
+    uint32_t            posW_org;
+    uint32_t            posH_org;
+
+    uint32_t            cur_framehandle;
+    uint32_t            screen;
+    uint32_t            currenthandle;
+    uint32_t            cur_frameid;
 } sun4i_hwc_layer_1_t;
 
-typedef struct hwc_context_t 
+typedef struct hwc_context_t
 {
     hwc_composer_device_1_t     device;
     hwc_procs_t             *procs;
@@ -61,10 +61,10 @@ typedef struct hwc_context_t
     bool                    hwc_layeropen;
     bool                    hwc_frameset;  /*is frame set*/
     bool                    hwc_reqclose;  /*is request close with parameter cmd*/
-    uint32_t				cur_hdmimode;
-	uint32_t				cur_3dmode;
-	bool					cur_half_enable;
-	bool					cur_3denable;
+    uint32_t                cur_hdmimode;
+    uint32_t                cur_3dmode;
+    bool                    cur_half_enable;
+    bool                    cur_3denable;
     /* our private state goes below here */
     bool                    wait_layer_open;
     bool                    vsync_enabled;
@@ -82,4 +82,5 @@ typedef struct hwc_context_t
 extern "C" int clock_nanosleep(clockid_t clock_id, int flags,
                            const struct timespec *request,
                            struct timespec *remain);
+
 #endif
