178edfa13dc872fbc3dbd394214a6b14d95d39d4
ugers <yugers@gmail.com>
2013-04-07 15:24:19 +0400

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -32,6 +32,7 @@ PRODUCT_RELEASE_NAME := 907
 UTC_DATE := $(shell date +%s)
 DATE     := $(shell date +%Y%m%d)
 
+#Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=nakasi TARGET_DEVICE=grouper BUILD_FINGERPRINT="google/nakasi/grouper:4.2.2/JDQ39/402395:user/release-keys" PRIVATE_BUILD_DESC="nakasi-user 4.2.2 JDQ39 402395 release-keys"
 
 # Allow ADB (to access dev settings)
