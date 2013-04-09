a8029023f4a335988ddf9240797a06325414fe74
ugers <yugers@gmail.com>
2013-04-09 14:55:23 +0400

    exp!:in builder i found:this app need to telephony

--- 907.mk
+++ 907.mk
@@ -98,7 +98,8 @@ PRODUCT_COPY_FILES += \
 	packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:/system/etc/permissions/android.software.live_wallpaper.xml
 
 PRODUCT_PACKAGES += \
-        Tag
+        Tag \
+        Mms
 
 PRODUCT_PACKAGES += \
 	librs_jni
