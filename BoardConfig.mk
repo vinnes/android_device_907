1c30945268cb55b1346569d67f912a5c380b7abd
ugers <yugers@gmail.com>
2013-05-01 19:11:10 +0400

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -66,11 +66,11 @@ TARGET_BOOTANIMATION_USE_RGB565 := true
 #BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/softwinner/907/recovery_keys.c
 BOARD_UMS_LUNFILE := "/sys/class/android_usb/android0/f_mass_storage/lun/file"
 BOARD_UMS_2ND_LUNFILE := "/sys/class/android_usb/android0/f_mass_storage/lun1/file"
-#TARGET_RECOVERY_INITRC := device/softwinner/907/recovery_init.rc
+#TARGET_RECOVERY_INITRC := device/softwinner/907/init.recovery.sun4i.rc
 
 #TWRP recovery
 #BOARD_HAS_SDCARD_INTERNAL := true
-#TARGET_RECOVERY_INITRC := device/softwinner/907/recovery_init.rc
+#TARGET_RECOVERY_INITRC := device/softwinner/907/init.recovery.sun4i.rc
 #TARGET_RECOVERY_PIXEL_FORMAT := "RGB_565"
 #DEVICE_RESOLUTION := 1024x768
 #TW_FLASH_FROM_STORAGE := true
