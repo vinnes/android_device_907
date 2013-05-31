50b5804feff6cd2954a19540f8f2f831f8258fc6
ugers <yugers@gmail.com>
2013-05-31 13:19:15 +0400

    Delete wifi-direct. I think this is one of reasons why wifi not stable

--- 907.mk
+++ 907.mk
@@ -74,7 +74,6 @@ PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
 	frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
 	frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
-        frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
 	frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
 	frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
 	frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
@@ -113,7 +112,7 @@ PRODUCT_COPY_FILES += \
 	device/softwinner/907/prebuilt/lib/cedarx/libswa1.so:system/lib/libswa1.so \
 	device/softwinner/907/prebuilt/lib/cedarx/libswa2.so:system/lib/libswa2.so \
 
-#wlan
+#Wlan
 PRODUCT_PACKAGES += \
 	libnetcmdiface
 
