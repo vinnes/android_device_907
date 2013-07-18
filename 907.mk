3088c8f95cf17b3570712ad0aa1c2b4adb26ed95
ugers <yugers@gmail.com>
2013-07-19 03:50:16 +0600

    Update 907.mk

--- 907.mk
+++ 907.mk
@@ -120,7 +120,7 @@ PRODUCT_PACKAGES += \
 	audio.primary.exDroid \
 	audio.a2dp.default \
 	audio.usb.default \
-	display.sun4i \
+	display.exDroid \
 	gralloc.sun4i \
 	power.sun4i \
 	camera.exDroid \
