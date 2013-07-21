65c8a3660bb68a4230f2f1975345477da328e658
ugers <yugers@gmail.com>
2013-07-21 15:30:37 +0600

    Update aokp_907.mk

--- aokp_907.mk
+++ aokp_907.mk
@@ -18,10 +18,6 @@ PRODUCT_BRAND := Google
 PRODUCT_DEVICE := 907
 PRODUCT_MODEL := Nexus 7
 PRODUCT_MANUFACTURER := Asus
-PRODUCT_RELEASE_NAME := 907
-
-UTC_DATE := $(shell date +%s)
-DATE     := $(shell date +%Y%m%d)
 
 #Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=nakasi TARGET_DEVICE=grouper BUILD_FINGERPRINT="google/nakasi/grouper:4.2.2/JDQ39E/402395:user/release-keys" PRIVATE_BUILD_DESC="nakasi-user 4.2.2 JDQ39E 402395 release-keys"
