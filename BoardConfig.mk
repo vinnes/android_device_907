e51d008a5ebb36c59c4beda314a4b0bc2fb27736
ugers <yugers@gmail.com>
2013-08-04 09:53:15 +0000

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -40,9 +40,10 @@ ARCH_ARM_HAVE_TLS_REGISTER := true
 
 #Bluetooth and Vibro stuff
 BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+BLUETOOTH_HCI_USE_USB := true
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/softwinner/907/bluetooth
 BOARD_BLUEDROID_VENDOR_CONF := device/softwinner/907/bluetooth/vnd_generic_usb.txt
-BLUETOOTH_HCI_USE_USB := true
 BOARD_HAS_VIBRATOR_IMPLEMENTATION := ../../device/softwinner/907/vibrator.c
 
 # Partition sizes; must be in decimal
