81a533367b2c0f0de7b1e20e953e259950505f32
ugers <yugers@gmail.com>
2014-01-05 02:02:28 +0300

    first commits

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -32,6 +32,7 @@ TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_VARIANT := cortex-a8
 TARGET_ARCH_VARIANT_CPU := cortex-a8
 TARGET_GLOBAL_CFLAGS += -mtune=cortex-a8 -mfpu=neon -mfloat-abi=softfp
 TARGET_GLOBAL_CPPFLAGS += -mtune=cortex-a8 -mfpu=neon -mfloat-abi=softfp
