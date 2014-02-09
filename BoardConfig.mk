d809787990c2faab02b66846e92ee2990e45d862
ugers <yugers@gmail.com>
2014-02-09 19:28:54 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -35,10 +35,10 @@ TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_VARIANT := cortex-a8
 TARGET_ARCH_VARIANT_CPU := cortex-a8
 ARCH_ARM_HAVE_NEON := true
+ARCH_ARM_HAVE_ARMV7A := true
 ARCH_ARM_HAVE_TLS_REGISTER := true
 TARGET_GLOBAL_CFLAGS += -mtune=cortex-a8 -mfpu=neon -mfloat-abi=softfp
 TARGET_GLOBAL_CPPFLAGS += -mtune=cortex-a8 -mfpu=neon -mfloat-abi=softfp
-
 TARGET_ARCH_LOWMEM := true
 
 #Bluetooth and Vibro stuff
