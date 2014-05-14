ed9fa5f9a5dc170e42e64df338dc1fa28010f28b
ugers <yugers@gmail.com>
2014-05-14 19:25:18 +0400

    Changed TARGET_ARCH_VARIANT_CPU to TARGET_CPU_VARIANT as it changed in 4.3

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -38,7 +38,6 @@ TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_VARIANT := cortex-a8
-TARGET_ARCH_VARIANT_CPU := cortex-a8
 ARCH_ARM_HAVE_TLS_REGISTER := true
 TARGET_GLOBAL_CFLAGS += -mtune=cortex-a8 -mfpu=neon -mfloat-abi=softfp
 TARGET_GLOBAL_CPPFLAGS += -mtune=cortex-a8 -mfpu=neon -mfloat-abi=softfp
