33466a45a2661d8d6dff1f6e3424220b6bffefb5
ugers <yugers@gmail.com>
2015-01-19 13:33:49 +0300

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -20,9 +20,6 @@ $(call inherit-product, device/softwinner/907/907-blobs.mk)
 # Inherit some common CyanogenMod stuff.
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 
-# Inherit GSM common stuff
-$(call inherit-product, vendor/cm/config/telephony.mk)
-
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := cm_907
 PRODUCT_BRAND := Google
@@ -35,7 +32,7 @@ UTC_DATE := $(shell date +%s)
 DATE     := $(shell date +%Y%m%d)
 
 #Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=nakasi TARGET_DEVICE=grouper BUILD_FINGERPRINT="google/nakasi/grouper:4.4.2/KOT49H/402395:user/release-keys" PRIVATE_BUILD_DESC="nakasi-user 4.4.2 KOT49H 402395 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=nakasi TARGET_DEVICE=grouper BUILD_FINGERPRINT="google/nakasi/grouper:5.0.2/LRX22G/402395:user/release-keys" PRIVATE_BUILD_DESC="nakasi-user 5.0.2 LRX22G 402395 release-keys"
 
 # Allow ADB (to access dev settings)
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.adb.secure=0 ro.secure=0 ro.allow.mock.location=1 service.adb.enable=1 persist.sys.usb.config=mass_storage ro.debuggable=1 persist.service.adb.enable=1 rild.libpath=/system/lib/liballwinner-ril.so
